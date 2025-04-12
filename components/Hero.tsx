c89ee828d283b90c9fc54dab4b0f315bef237d87
darcy <darcy.ldx@gmail.com>
2025-04-11 21:43:16 -0400

    Hero  h-[50vh]
            {/*
            <section className="mb-24 flex justify-center">
              <QRCode/>
            </section>
            */}

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export function Hero() {
   return (
-    <div className="relative w-full h-[33vh] bg-gradient-to-r from-black to-gray-800 flex items-center justify-center">
+    <div className="relative w-full h-[50vh] bg-gradient-to-r from-black to-gray-800 flex items-center justify-center">
       {/* Make sure content is visible despite navbar overlay */}
       <div className="absolute inset-0 z-0">
         {/* Background content (if any) */}
